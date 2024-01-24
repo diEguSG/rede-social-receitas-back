624b17e09b3108e07d994db4cbe6cb40f3fa3716
diEguSG <diegooosgarcia@gmail.com>
2024-01-24 20:25:40 -0300

    Adição Cors

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import {connection} from "./server.js";
 import express from "express";
-import {user_router} from "./rotas/usuario.rotas.js"
+import {user_router} from "./rotas/usuario.rotas.js";
+import cors from "cors";
 
 connection.connect(console.log("Banco Conectado"));
 
 const app = express()
 
+app.use(cors());
 app.use(express.json());
 app.use("/user", user_router);
 
