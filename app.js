e0e3787e2f6ac18eedf37276b66a5cf95803bf63
diEguSG <diegooosgarcia@gmail.com>
2024-01-26 20:08:01 -0300

    Alteração nome da rota

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express()
 
 app.use(cors());
 app.use(express.json());
-app.use("/user", user_router);
+app.use("/usuario", user_router);
 
 app.listen(3003, console.log("Server Online"))
 
