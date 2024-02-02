160d77dff40cdba9e4035986fe5ba819230150ee
diEguSG <diegooosgarcia@gmail.com>
2024-02-01 21:23:56 -0300

    Gerador de token de usuário com o módulo JWT

--- controller/usuario.controller.js
+++ controller/usuario.controller.js
@@ -1,3 +1,5 @@
+import {env} from "node:process";
+import jwt from "jsonwebtoken";
 import {seleciona_usuario_model} from "../models/usuario.model.js"
 import {criar_usuario_model} from "../models/usuario.model.js";
 import {atualizar_usuario_model} from "../models/usuario.model.js"
@@ -9,8 +11,27 @@ export async function seleciona_usuario_controller(req, res){
         senha: req.body.senha
     }
 
+
+
     const usuario = await seleciona_usuario_model(dados);
-    return res.json(usuario);
+
+    if(usuario.length == 0){
+        return res.status(404).json({Erro: "Login Inválido"})
+    }
+
+    console.log(env.SECRETKEY);
+
+    const token = jwt.sign({
+        tipo_usuario: usuario[0].tipo_usuario,
+        id_usuario: usuario[0].id
+    },
+    "Chave Secreta",{
+        expiresIn: "24h",
+        subject: String(usuario[0].id)
+    }
+    )
+
+    return res.status(200).json({acesso_token: token});
 }
 
 export async function criar_usuario_controller(req, res){
